24eff827c34cb33180c17fed27c786e725ff08b8
nancyyadav2217 <102166060+nancyyadav2217@users.noreply.github.com>
2022-09-21 22:19:51 +0530

    Add files via upload

---
+++ day2_assignment/MinMaxNumb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class MinMaxNumb
+    {
+        public static void Main()
+        {
+            int n;
+
+            Console.WriteLine("Enter the array number to be stored: ");
+            n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            Console.WriteLine("Enter the array values");
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Maximum number is {numbers.Max()} ");
+            Console.WriteLine($"Minimum number is {numbers.Min()} ");
+        }
+    }
+}
